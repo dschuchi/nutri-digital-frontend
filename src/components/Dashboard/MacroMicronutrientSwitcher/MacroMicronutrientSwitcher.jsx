ad29a2719650016cb5bc7578340e1db5192fde67
Franz <favendano@fi.uba.ar>
2025-05-20 22:33:13 -0300

    Se mejoro UX del componente MircroMacroNutrientsSwitcher

--- src/components/Dashboard/MacroMicronutrientSwitcher/MacroMicronutrientSwitcher.jsx
+++ src/components/Dashboard/MacroMicronutrientSwitcher/MacroMicronutrientSwitcher.jsx
@@ -1,23 +1,30 @@
 import { useState } from 'react';
 import MacroCard from './MacroCard';
 import MicronutrientsCard from './MicronutrientsCard';
+import { SwapOutlined } from '@ant-design/icons';
 import './MacroMicronutrientSwitcher.css';
 
 const MacroMicronutrientSwitcher = ({ macros, micros }) => {
   const [mostrarMicros, setMostrarMicros] = useState(false);
 
+  const handleSwitch = () => {
+    setMostrarMicros((prev) => !prev);
+  };
+
   return (
     <div
       className="card-switcher-container"
-      onClick={() => setMostrarMicros((prev) => !prev)}
-      style={{ cursor: 'pointer' }}
+      onClick={handleSwitch}
     >
-      <div className={`switch-card ${!mostrarMicros ? 'visible' : 'hidden'}`}>
-        <MacroCard data={macros} />
+      <div className="switch-icon">
+        <SwapOutlined />
       </div>
-      <div className={`switch-card ${mostrarMicros ? 'visible' : 'hidden'}`}>
+
+      {mostrarMicros ? (
         <MicronutrientsCard data={micros} />
-      </div>
+      ) : (
+        <MacroCard data={macros} />
+      )}
     </div>
   );
 };
