50addf47f5f14e4afd1a8e0a5535390001b68fb5
sschuchinsky <s.schuchinsky@gmail.com>
2025-05-31 00:21:15 -0300

    Agrega el componente MacroGoals para gestionar los objetivos de macronutrientes y elimina la lógica redundante en Goals

--- src/pages/Goals.jsx
+++ src/pages/Goals.jsx
@@ -1,28 +1,16 @@
-import { Button, Flex, Form, Input, InputNumber, List, Typography } from "antd";
+import { Button, Flex, Form, InputNumber, List, Typography } from "antd";
 import { useEffect, useState } from "react";
-import { getMacroNutrientGoals, getMicroNutrientGoals, updateMacroNutrientGoals, updateMicroNutrientGoals } from "../api/nutrientGoals";
+import { getMicroNutrientGoals, updateMicroNutrientGoals } from "../api/nutrientGoals";
 import { useAuth } from "../context/AuthContext";
+import MacroGoals from "../components/goals/MacroGoals";
 
 const Goals = () => {
     const [microNutrientGoals, setMicroNutrientGoals] = useState([]);
-    const [macroNutrientGoals, setMacroNutrientGoals] = useState([]);
     const [activityGoals, setActivityGoals] = useState([]);
-    const [editMacro, setEditMacro] = useState(false);
     const [editMicro, setEditMicro] = useState(false);
     const { user } = useAuth();
 
     useEffect(() => {
-        getMacroNutrientGoals(user.id)
-            .then((res) => {
-                setMacroNutrientGoals([
-                    { name: 'Calorías', value: res.data[0].calories, unit: '', key: 'calories' },
-                    { name: 'Carbohidratos', value: res.data[0].total_carbs, unit: '%', key: 'total_carbs' },
-                    { name: 'Grasas', value: res.data[0].total_fat, unit: '%', key: 'total_fat' },
-                    { name: 'Proteínas', value: res.data[0].protein, unit: '%', key: 'protein' },
-                ]);
-            })
-            .catch((err) => { console.error("Error fetching nutrient goals:", err) });
-
         getMicroNutrientGoals(user.id)
             .then((res) => {
                 setMicroNutrientGoals([
@@ -44,32 +32,7 @@ const Goals = () => {
             .catch((err) => { console.error("Error fetching nutrient goals:", err) });
     }, []);
 
-    const [formMacro] = Form.useForm();
     const [formMicro] = Form.useForm();
-
-    const handleSaveMacro = () => {
-        formMacro
-            .validateFields()
-            .then((values) => {
-                updateMacroNutrientGoals(values)
-                    .then((res) => {
-                        console.log("Nutrient goals updated successfully:", res);
-                        setMacroNutrientGoals([
-                            { name: 'Calorías', value: res.data[0].calories, unit: 'kcal', key: 'calories' },
-                            { name: 'Carbohidratos', value: res.data[0].total_carbs, unit: '%', key: 'total_carbs' },
-                            { name: 'Grasas', value: res.data[0].total_fat, unit: '%', key: 'total_fat' },
-                            { name: 'Proteínas', value: res.data[0].protein, unit: '%', key: 'protein' },
-                        ]);
-                    })
-                    .catch((err) => {
-                        console.error("Error updating nutrient goals:", err);
-                    });
-                setEditMacro(false);
-            })
-            .catch((info) => {
-                console.log('Validación fallida:', info);
-            });
-    };
 
     const handleSaveMicro = () => {
         formMicro
@@ -105,16 +68,6 @@ const Goals = () => {
     }
 
     useEffect(() => {
-        if (macroNutrientGoals.length > 0) {
-            const formValues = Object.fromEntries(
-                macroNutrientGoals.map(item => [item.key, item.value])
-            );
-            formMacro.setFieldsValue(formValues);
-        }
-    }, [macroNutrientGoals, formMacro]);
-
-
-    useEffect(() => {
         if (microNutrientGoals.length > 0) {
             const formValues = Object.fromEntries(
                 microNutrientGoals.map(item => [item.key, item.value])
@@ -123,23 +76,6 @@ const Goals = () => {
         }
     }, [microNutrientGoals, formMicro]);
 
-    const validateMacroSum = (_, value) => {
-        const values = formMacro.getFieldsValue();
-        const total =
-            Number(values.total_carbs || 0) +
-            Number(values.total_fat || 0) +
-            Number(values.protein || 0);
-        if (
-            values.total_carbs !== undefined &&
-            values.total_fat !== undefined &&
-            values.protein !== undefined
-        ) {
-            if (total !== 100) {
-                return Promise.reject('La suma de carbohidratos, grasas y proteínas debe ser exactamente 100%');
-            }
-        }
-        return Promise.resolve();
-    };
 
     return (
         <div>
@@ -148,58 +84,7 @@ const Goals = () => {
                 <div style={{ width: "40%" }}>
                     <Flex vertical gap='large'>
                         <div>
-                            <Flex justify="space-between" align="center">
-                                <Typography.Title level={4}>Objetivos de nutrición diarios</Typography.Title>
-                                <div>
-                                    {editMacro ? (
-                                        <>
-                                            <Button onClick={handleSaveMacro}>Guardar</Button>
-                                            <Button onClick={() => setEditMacro(false)}>Cancelar</Button>
-                                        </>
-                                    ) : (
-                                        <Button onClick={() => setEditMacro(true)}>Editar</Button>
-                                    )}
-                                </div>
-                            </Flex>
-                            <Form
-                                form={formMacro}
-                                name="macro-goals">
-                                <List
-                                    bordered
-                                    dataSource={macroNutrientGoals}
-                                    renderItem={(item) => (
-                                        <List.Item>
-                                            <div>
-                                                <Typography.Text strong>
-                                                    {item.name}
-                                                </Typography.Text>
-                                            </div>
-                                            <div>
-                                                {editMacro ? (
-                                                    <Form.Item
-                                                        name={item.key}
-                                                        style={{ margin: 0 }}
-                                                        rules={[
-                                                            { required: true, message: 'Campo requerido' },
-                                                            { type: 'number', min: 1, message: 'Debe ser mayor a 1' },
-                                                            ...(item.key === 'total_carbs' || item.key === 'total_fat' || item.key === 'protein'
-                                                                ? [{ validator: validateMacroSum }]
-                                                                : [])
-                                                        ]}
-                                                    >
-                                                        <InputNumber suffix={item.unit} />
-                                                    </Form.Item>
-                                                ) : (
-                                                    <span>
-                                                        {item.value} {item.unit}
-                                                    </span>
-                                                )}
-                                            </div>
-                                        </List.Item>
-                                    )}
-                                />
-                            </Form>
-
+                            <MacroGoals />
                         </div>
 
                         <div>
