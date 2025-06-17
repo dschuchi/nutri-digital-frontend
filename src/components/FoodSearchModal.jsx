b40aaea88b0ac00213ec10df3c8974670f0deb5f
Franz <favendano@fi.uba.ar>
2025-06-17 18:15:51 -0300

    Se mejoro el espaciado del modal de busqueda de alimentos para planificacion

--- src/components/FoodSearchModal.jsx
+++ src/components/FoodSearchModal.jsx
@@ -1,8 +1,9 @@
-// components/FoodSearchModal.jsx
-import { Modal } from 'antd';
 import FoodSearchContent from './FoodSearchContent';
+import { Modal, Typography } from 'antd';
+
 
 const FoodSearchModal = ({ open, onClose, onSelect }) => {
+  const { Title } = Typography;
   return (
     <Modal
       open={open}
@@ -10,6 +11,8 @@ const FoodSearchModal = ({ open, onClose, onSelect }) => {
       footer={null}
       width={1000}
       destroyOnClose
+      title={<Title level={4} style={{ margin: 0 }}>Buscar alimento</Title>}
+      bodyStyle={{ paddingTop: 12, paddingBottom: 24, paddingLeft: 24, paddingRight: 24 }}
     >
       <FoodSearchContent onSelect={onSelect} />
     </Modal>
